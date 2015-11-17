ab466993d6ffce26ddda2d13b9092355f873741c
TakumaKobayashi <primaryload27@outlook.jp>
2015-11-17 17:13:28 +0900

    modified common.h

--- server/server_common.h
+++ server/server_common.h
@@ -9,7 +9,10 @@ mapData MAP;
 
 /* server_net.c */
 extern int gClientNum;
-
+extern Charatype gChara[CT_NUM];
+extern ITEM_NUMBER Item[ITEM_NUM];
+extern PLAYER player;
+extern POSITION positon;
 #define ALL_CLIENTS	-1   
 
 #endif
