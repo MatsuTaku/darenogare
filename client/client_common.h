b0ce4a5a6dabca4aea26be0902986a0528b0e6bf
MatsuTaku <matsumoto@livnest.co.jp>
2015-12-10 18:47:14 +0900

    change speed values

--- client/client_common.h
+++ client/client_common.h
@@ -8,10 +8,10 @@
 #define PI				3.14159265
 #define HALF_DEGRESS	180	
 #define ANGULAR_VEROCITY	360	// 角速度
-#define ACCELE_GO	500		// 加速度
-#define ACCELE_BRAKE	-400	// 減速度
-#define MAXIMUM_SPEED	2000
-#define MAXIMUM_SPEED_OBSTACLE	500
+#define ACCELE_GO	200		// 加速度
+#define ACCELE_BRAKE	-160	// 減速度
+#define MAXIMUM_SPEED	800
+#define MAXIMUM_SPEED_OBSTACLE	200
 // #define RESISTANCE		-0	// 抵抗
 
 
